c22438755658e1caa2e99455339a2e8d371e5aa2
Ilya Serov <bratiliya@gmail.com>
2017-01-21 22:14:08 +0300

    Update settings.py

--- Django_template/settings.py
+++ Django_template/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q(=!zd55ycq(1ebo6en=bn)@jfhj1vpwqdeu$m^zc(nb#0j4!z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
